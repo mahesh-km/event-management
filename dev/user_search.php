c721accbca3de6ee6657aef5e606f0fa90d5a812
Mahesh K M <mahesh@vyoma-media.in>
2017-02-04 16:17:44 +0530

    modified script

--- dev/user_search.php
+++ dev/user_search.php
@@ -6,6 +6,9 @@ $username = "root";
 $password = "root";
 $dbname = "mascotautomobiles";
 
+$delarname = $_POST['delar_name'];
+$delaremail = $_POST['delar_email'];
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 //check connection
@@ -13,27 +16,60 @@ if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
 }
 
-
-$query_select = "SELECT `delar_id`, `delar_name`, `email`, `phone_mob`, `land_phone`, `delar_dist`, `pass` FROM `Register` WHERE `delar_id` = '$_POST[delar_id]'";
+if(!isset($delarname) || trim($delarname) == ''){
+	if (!isset($delaremail) || trim($delaremail) == ''){
+        	echo "<script type='text/javascript'>";
+                echo "alert('Please input some valid data in to the filed');";
+                echo 'window.location.href = "user_search.html";';
+                echo "</script>";
+	}
+        else {
+        	$query_select = "SELECT `delar_id`, `delar_name`, `email`, `phone_mob`, `land_phone`, `delar_dist`, `pass` FROM `Register` WHERE `email` REGEXP '$_POST[delar_email]'";
+        }
+}
+else {
+	$query_select = "SELECT `delar_id`, `delar_name`, `email`, `phone_mob`, `land_phone`, `delar_dist`, `pass` FROM `Register` WHERE `delar_name` REGEXP '$_POST[delar_name]'";
+}
         
 $result = mysqli_query($conn, $query_select);
         
         if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-                        echo "<html>";
-                        echo "<link rel='stylesheet' type='text/css' href='CSS/style_merge.css'>";
-                        echo "<form action='http://mascotautomobiles.com/dev/user_search.html' >";
-                        echo "<ul>";
-                        echo "<li>Delar ID  : {$row['delar_id']} </li>";
-                        echo "<li>Delar Name: {$row['delar_name']} </li>";
-                        echo "<li>Email     : {$row['email']}</li>";
-                        echo "<li>Mobile    : {$row['phone_mob']}</li>";
-                        echo "<li>District  : {$row['delar_dist']}</li>";
-                        echo "<li>No. Tickets issued: {$row['pass']}</li>";
-                        echo "";
-                        echo "<button type='submit'>Done!</button>";
-                        echo "</form>";
-                        echo "</html>";
+                echo "<html>";
+                echo "<link rel='stylesheet' type='text/css' href='CSS/style_table.css'>";
+                echo "<form action='http://mascotautomobiles.com/dev/user_search.html' >";
+		echo "<table id='box-table-a' >";
+                        echo "<thead>";
+		        	echo "<tr>";
+        				echo "<th>Delar ID</th>";
+			                echo "<th>Name</th>";
+			                echo "<th>Email</th>";
+	                		echo "<th>Phone Number</th>";
+			                echo "<th>District</th>";
+			                echo "<th>No. pass issued</th>";
+	                	echo "</tr>"; 
+                 	echo "</thead>";
+                	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+	  		//while($row = $result->fetch_assoc()) {
+                        echo "<tbody>";
+	                        echo "<tr>";
+                	        	echo "<td>". $row['delar_id']. "</td>";
+        		                echo "<td>" . $row["delar_name"]. "</td>";
+		                        echo "<td>" . $row["email"]. "</td>";
+                		        echo "<td>" . $row["phone_mob"]. "</td>";
+                        		echo "<td>" . $row["delar_dist"]. "</td>";
+	                        	echo "<td>" . $row["pass"]. "</td>";	
+                        	echo "</tr>";
+                      	echo "</tbody>";
                 }
-        }   
+                echo "</table>";
+                echo "<button type='submit'>Done</button>";
+                echo "</form>";
+                echo "</html>";
+ 
+	}else {
+         echo "<script type='text/javascript'>";
+         echo "alert('No records found!');";
+         echo 'window.location.href = "user_search.html";';
+         echo "</script>";
+        } 
 
