c664a0fa46c61300421511cfdc5dae71f4e1951b
Mahesh K M <mahesh@vyoma-media.in>
2017-02-06 20:32:33 +0530

    schema.sql modified

--- schema.sql
+++ schema.sql
@@ -1,29 +1,39 @@
 
 # Register
 CREATE TABLE `Register` (
-  `delar_id` varchar(20) NOT NULL,
-  `delar_name` varchar(20) NOT NULL,
-  `contact_name` varchar(20),
-  `salesman_name` varchar(20),  	 
+  `delar_id` int(11) NOT NULL AUTO_INCREMENT,
+  `delar_name` varchar(80) NOT NULL,
+  `contact_name` varchar(20) DEFAULT NULL,
+  `salesman_name` varchar(20) DEFAULT NULL,
   `email` varchar(80) NOT NULL,
-  `address` char(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
   `phone_mob` bigint(10) NOT NULL,
-  `land_phone` bigint(12),
+  `land_phone` bigint(12) DEFAULT NULL,
   `delar_dist` char(50) NOT NULL,
-  `pass` int(4) NOT NULL,
-  `ticket_id` varchar(50) NOT NULL DEFAULT '',
+  `tin_no` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`delar_id`),
   UNIQUE KEY `email` (`email`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1
 
 # CheckIn
 CREATE TABLE `CheckIn` (
-  `checkin_id` int(11) NOT NULL AUTO_INCREMENT primary key,
-  `checkin_date` date,
-  `checkin_time` time,
-  `checkin_tkt` int(4),
-  `delar_id` varchar(20) NOT NULL,
+  `checkin_id` int(11) NOT NULL AUTO_INCREMENT,
+  `checkin_date` date DEFAULT NULL,
+  `checkin_time` time DEFAULT NULL,
+  `checkin_tkt` int(4) DEFAULT NULL,
+  `ticket_id` varchar(50) NOT NULL,
+  PRIMARY KEY (`checkin_id`),
+  KEY `fk_ticket_id` (`ticket_id`),
+  CONSTRAINT `fk_ticket_id` FOREIGN KEY (`ticket_id`) REFERENCES `Ticket_info` (`ticket_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+# Ticket_info
+CREATE TABLE `Ticket_info` (
+  `ticket_id` varchar(50) NOT NULL,
+  `pass_no` int(4) NOT NULL,
+  `ticket_date` date DEFAULT NULL,
+  `delar_id` int(11) NOT NULL,
+  PRIMARY KEY (`ticket_id`),
   KEY `fk_delar_id` (`delar_id`),
   CONSTRAINT `fk_delar_id` FOREIGN KEY (`delar_id`) REFERENCES `Register` (`delar_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-   	
+) ENGINE=InnoDB DEFAULT CHARSET=latin1   	
